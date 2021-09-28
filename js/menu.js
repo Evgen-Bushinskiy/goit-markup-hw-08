13c2bfeb5e1958384bc70330fca32c2631a47711
Evgen Bushinskiy <bushmc83@gmail.com>
2021-09-29 00:42:25 +0300

    Homework 8

--- js/menu.js
+++ js/menu.js
@@ -2,13 +2,13 @@
   const refs = {
     openMenuBtn: document.querySelector('.menu-open'),
     closeMenuBtn: document.querySelector('.menu-close'),
-    modal: document.querySelector('.menu-mob'),
+    menu: document.querySelector('.menu-mob'),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleMenu);
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
 
   function toggleMenu() {
-    refs.Menu.classList.toggle('is-hidden');
+    refs.menu.classList.toggle('is-hidden');
   }
 })();
